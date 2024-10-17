6550aeef845b0b066917c988a45c03558df46d3c
Mahmoud Hamdan <mah63413@gmail.com>
2024-10-17 12:51:13 +0300

    image & login added

--- diabetes/test/widget_test.dart
+++ diabetes/test/widget_test.dart
@@ -8,7 +8,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:diabetes/main.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter App'),
+        ),
+        body: const Center(
+          child: Text('0'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
