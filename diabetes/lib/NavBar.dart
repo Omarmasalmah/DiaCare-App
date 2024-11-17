48ce32db24bdf46fb887fb5aca85c3f5437a9cc3
Mahmoud Hamdan <mah63413@gmail.com>
2024-11-17 22:48:28 +0200

    Nov 17th

--- diabetes/lib/NavBar.dart
+++ diabetes/lib/NavBar.dart
@@ -1,9 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:diabetes/pages/FAQpage.dart';
 import 'package:diabetes/pages/home_screen.dart';
+import 'package:diabetes/pages/login.dart';
 
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
+
+  Future<Map<String, dynamic>?> getUserProfile() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot userProfile = await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(user.uid)
+          .get();
+      return userProfile.data() as Map<String, dynamic>?;
+    }
+    return null;
+  }
+
+  void _signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => LogIn()),
+      (Route<dynamic> route) => false,
+    );
+  }
+
+  void _showSignOutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Sign Out"),
+          content: const Text("Are you sure you want to sign out?"),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("No"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+              },
+            ),
+            TextButton(
+              child: const Text("Yes"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the dialog
+                _signOut(context); // Sign out the user
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,26 +62,100 @@ class NavBar extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          const UserAccountsDrawerHeader(
-            accountName: Text('Mahmoud Hamdan'),
-            accountEmail: Text('1201134@student.birzeit.edu'),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
-                child: Image(
-                  image: AssetImage('images/profile.jpg'),
-                  width: 90,
-                  height: 90,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              image: DecorationImage(
-                image: AssetImage('images/backProfile.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
+          FutureBuilder<Map<String, dynamic>?>(
+            future: getUserProfile(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const UserAccountsDrawerHeader(
+                  accountName: Text('Loading...'),
+                  accountEmail: Text('Loading...'),
+                  currentAccountPicture: CircleAvatar(
+                    child: ClipOval(
+                      child: Image(
+                        image: AssetImage('images/profile.jpg'),
+                        width: 90,
+                        height: 90,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                      image: AssetImage('images/backProfile.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return const UserAccountsDrawerHeader(
+                  accountName: Text('Error'),
+                  accountEmail: Text('Error'),
+                  currentAccountPicture: CircleAvatar(
+                    child: ClipOval(
+                      child: Image(
+                        image: AssetImage('images/profile.jpg'),
+                        width: 90,
+                        height: 90,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                      image: AssetImage('images/backProfile.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              } else if (snapshot.hasData) {
+                var userProfile = snapshot.data;
+                return UserAccountsDrawerHeader(
+                  accountName: Text(userProfile?['name'] ?? 'No Name'),
+                  accountEmail: Text(userProfile?['email'] ?? 'No Email'),
+                  currentAccountPicture: CircleAvatar(
+                    child: ClipOval(
+                      child: Image(
+                        image: AssetImage('images/profile.jpg'),
+                        width: 90,
+                        height: 90,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                      image: AssetImage('images/backProfile.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              } else {
+                return const UserAccountsDrawerHeader(
+                  accountName: Text('No User'),
+                  accountEmail: Text('No User'),
+                  currentAccountPicture: CircleAvatar(
+                    child: ClipOval(
+                      child: Image(
+                        image: AssetImage('images/profile.jpg'),
+                        width: 90,
+                        height: 90,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                      image: AssetImage('images/backProfile.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              }
+            },
           ),
           ListTile(
             title: const Text('Home'),
@@ -76,7 +201,7 @@ class NavBar extends StatelessWidget {
           ListTile(
             title: const Text('Log Out'),
             leading: const Icon(Icons.logout),
-            onTap: () => null,
+            onTap: () => _showSignOutDialog(context),
           ),
           // Add more ListTiles for other navigation items here
         ],
